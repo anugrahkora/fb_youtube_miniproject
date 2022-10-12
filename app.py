690c90526a134b8d1217490b1ed9c8f9fe1f6551
Anugrah K <anugrahkorembeth.ak@gmail.com>
2022-10-12 15:36:05 +0530

    minor updates

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
-from credentials import token,client_secret,client_id,password,user_name,user_agent
+from turtle import title
+from credentials import token, client_secret, client_id, password, user_name, user_agent, keywords
 import facebook
+import csv
 import praw
+from prawcore.exceptions import Forbidden
+from convert import listToString
 
 reddit = praw.Reddit(
     client_id=client_id,
@@ -9,12 +13,39 @@ reddit = praw.Reddit(
     user_agent=user_agent,
     username=user_name,
 )
+data = []
+header = ['id', 'name', 'title', 'score', 'url', 'created_time']
+count = 0
 
-# print(reddit.user.me())
-# reddit.read_only=True
+for keys in keywords:
+    try:
+        for submission in reddit.subreddit("all").search(listToString(keys), limit=None):
 
-for submission in reddit.subreddit("test").hot(limit=10):
-    print(submission.title)
+            data.append([submission.id, submission.name, submission.title,
+                        submission.score, submission.url,  submission.created_utc])
+            count += 1
+            print(count)
+    except Forbidden:
+        print("We\'ve been banned on "+listToString(keys))
+
+
+
+with open('reddit.csv', 'w', encoding='UTF8', newline='') as f:
+    writer = csv.writer(f)
+
+    # write the header
+    writer.writerow(header)
+
+    # write the data
+    writer.writerows(data)
+ 
+
+
+
+
+
+
+# print(len(titles))
 # postIds = ['5302983239800416', '1178170342746546']
 # fetchedDescriptions = []
 # graph = facebook.GraphAPI(access_token=token, version=2.0)
@@ -25,8 +56,6 @@ for submission in reddit.subreddit("test").hot(limit=10):
 #     fetchedDescriptions.append(res)
 
 # print(fetchedDescriptions[0]['description'])
-
-
 
 
 # events = graph.request('search', {'q': 'aquafresh', 'type': 'page'})
